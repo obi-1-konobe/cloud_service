5690c3ecd4c072d2113354a9c37f116774d8a21f
obi-1-konobe <obi1konobe@gmail.com>
2020-04-19 17:30:22 +0300

    start project, add server app

---
+++ microservices/server/app/main.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+storage = [
+    {
+        'datetime': '12/12/12 12:12:12',
+        'heartbeats': '120',
+        'temperature': '36.6'
+    }
+]
+
+
+class Metrics(BaseModel):
+    datetime: str
+    heartbeats: str
+    temperature: str
+
+
+@app.get('/', response_model=List[Metrics])
+async def index():
+    return storage
+
