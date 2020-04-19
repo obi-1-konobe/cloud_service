9ae4357fe39fc1144048b2e70328ec245616723e
obi-1-konobe <obi1konobe@gmail.com>
2020-04-19 18:21:55 +0300

    add requirements.txt

--- microservices/device/app/run.py
+++ microservices/device/app/run.py
@@ -1,18 +1,21 @@
 import random
 import json
 import requests
+import time
 from datetime import datetime
 
-date_time = "{:%d/%m/%Y %H:%M:%S}".format(datetime.now())
-temperature = round(random.uniform(35.0, 37.0), 1)
-heartbeat = random.randint(110, 140)
+while True:
+    date_time = "{:%d/%m/%Y %H:%M:%S}".format(datetime.now())
+    temperature = round(random.uniform(35.0, 37.0), 1)
+    heartbeat = random.randint(110, 140)
 
-data = {
-    'datetime': date_time,
-    'temperature': f'{temperature}',
-    'heartbeats': f'{heartbeat}'
-}
-data_json = json.dumps(data)
-payload = {'payload': data_json}
-r = requests.post('http://localhost:8000/', data=data_json)
-print(r)
+    data = {
+        'datetime': date_time,
+        'temperature': f'{temperature}',
+        'heartbeats': f'{heartbeat}'
+    }
+    data_json = json.dumps(data)
+    payload = {'payload': data_json}
+    r = requests.post('http://localhost:8000/', data=data_json)
+    print(r)
+    time.sleep(10)
